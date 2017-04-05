77622dd5e6e81ff92884e251da81aae6441917e1
Thomas Chao <techao@gmail.com>
2017-04-05 04:25:18 -0700

    Add Code for Hamster's name

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,10 +1,17 @@
-puts "Hello, does your hamster have a name?"
-answer = gets.chomp
+p "Hello, does the hamster you would like to admit today have a name? Please answer with a yes or no."
+		answer = gets.chomp
+		if answer == "yes"
+		
+			puts "What is the name of your hamster?"
+			ham_name = gets.chomp
+		else answer == "no"
+			
+			puts "Okay, please give the hamster a name to continue."
+			ham_name = gets.chomp
+		end
+p "Okay. Hello there #{ham_name}"
 
 
 
-if name_yes_no = "yes"
-	puts "What is the name of your hamster"
-elsif 
-	puts 
-	
+
+
