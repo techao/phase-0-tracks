824e1ad185e30059e35ed5e3f61f4b9790d5638b
Thomas Chao <techao@gmail.com>
2017-04-06 22:51:50 -0700

    Write Pseudocode for Encrypting Method

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,14 @@
+# write a method to encrypt a string
+# define the method
+# use a gets.chomp to retreieve text from user as string 
+# define variable for the word to be enrypted 
+# print string with each letter replaced by the one after it.
+# note: print word[index] will print the letter that 
+# corresponds to the inedx interger.
+
+# must wrrite a conditional method with loop in order to print
+# all of the string.
+#write conditiom if the index (as an interger)
+# is less than the length of the word (represented
+# as an interger) then word index is equal to the word's index
+
