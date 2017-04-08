e77de265abc73cc9b9d9360130684af35a78904e
Thomas Chao <techao@gmail.com>
2017-04-08 04:20:36 -0700

    Decryption Method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -12,7 +12,7 @@
 # is less than the length of the word (represented
 # as an interger) then word index is equal to the word's index
 
-# def encrypt_method
+# def encrypt_method2
 # 	puts "Enter text for encryption"
 # 	password_str = gets.chomp
 # 	index = 0
@@ -22,9 +22,9 @@
 # 	end
 # 	puts password_str
 # end
-# encrypt_method
+# encrypt_method2
 
-def encrypt_method2(password_str)
+def encrypt_method(password_str)
 	index = 0
 	while 
 	 index <  password_str.length
@@ -35,6 +35,22 @@ def encrypt_method2(password_str)
 end
 encrypt_method2("abcdefg")
 
+def decrypt_method(encrypted_str)
+index = 0 
+alpha_bet = "abcdefghijklmnopqrstuvwxyz"
+
+  while index < encrypted_str.length
+    if 
+      encrypted_str_indx = alpha_bet.index("#{encrypted_str[index]}")
+      decrypted_str_indx = encrypted_str_indx - 2
+    end
+   encrypted_str[index] = "#{alpha_bet[decrypted_str_indx]}".next
+   index +=1
+  end 
+  puts encrypted_str
+end
+decrypt_method("bcdefgh")
+
 # note: Both excrypt methods dont return spaces as spaces. 
 # not sure how to do that yet.
 
@@ -42,24 +58,25 @@ encrypt_method2("abcdefg")
 # how to find out where letter is in alphabt?
 # goal = decryp "wod" == "vnc"
 
-def decrypt_method
-  puts "Enter text for encryption"
-  password_str = gets.chomp
-  index = 0 
-  alpha_bet = "abcdefghijklmnopqrstuvwxyz"
+# def decrypt_method2
+#   puts "Enter text for encryption"
+#   password_str = gets.chomp
+#   index = 0 
+#   alpha_bet = "abcdefghijklmnopqrstuvwxyz"
   
- # result in 22 assigned to variable encrypted_str_indx from str 'w'
- # at this point index is = 21, now to apply index to alphabet. 21 is 1 less than 
- # than 22 so should retrieve one str before the original str taken
- # decrypted_string = v, now loop everything so it prints until condition of all encrypted letters are processed is met. 
- 	while index < password_text.length
-	 	if 
-	 	 encrypted_str_indx = alpha_bet.index("#{password_text[index]}")
-	 	 decrypted_str_indx = encrypted_str_indx - 2
-		end
-	 password_text[index] = "#{alpha_bet[decrypted_str_indx]}".next
-	 index +=1 
-	end
-	puts password_text
-end
+#  # result in 22 assigned to variable encrypted_str_indx from str 'w'
+#  # at this point index is = 21, now to apply index to alphabet. 21 is 1 less than 
+#  # than 22 so should retrieve one str before the original str taken
+#  # decrypted_string = v, now loop everything so it prints until condition of all encrypted letters are processed is met. 
+#  	while index < password_text.length
+# 	 	if 
+# 	 	 encrypted_str_indx = alpha_bet.index("#{password_text[index]}")
+# 	 	 decrypted_str_indx = encrypted_str_indx - 2
+# 		end
+# 	 password_text[index] = "#{alpha_bet[decrypted_str_indx]}".next
+# 	 index +=1 
+# 	end
+# 	puts password_text
+# end
+# decrypt_method2
 
