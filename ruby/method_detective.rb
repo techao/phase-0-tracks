1e928e8e8811824eff7a9d203ef4daf896d6c5a9
Thomas Chao <techao@gmail.com>
2017-04-10 05:28:22 -0700

    identify metods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,42 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+"zom".inser(2, o)
+=> “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"enhance".center
+=> "    enhance    "
+.lsut
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".capitalize
+=> "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+"the usual" + suspects.to_s
+=> "the usual suspects"
+.replace
 
-# " suspects".<???>
-# => "the usual suspects"
+" suspects".replace
+=> "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
+.chomp
+.delete
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+"The mystery of the missing first letter".delete
+=> "he mystery of the missing first letter"
 
-# "z".<???>
-# => 122 
+"Elementary,    my   dear        Watson!".strip
+=> "Elementary, my dear Watson!"
+
+"z".gsub
+=> 122
+.sub
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"How many times does the letter 'a' appear in this string?".count "a"
+=> 4
