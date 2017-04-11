0186b96b282027f39a50bc6d47852b4b7fd1eff0
Thomas Chao <techao@gmail.com>
2017-04-10 21:16:27 -0700

    Update answers

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,22 +7,22 @@
 "iNvEsTiGaTiOn".swapcase
 => “InVeStIgAtIoN”
 
-"zom".inser(2, o)
+"zom".insert(2, "o")
 => “zoom”
 
-"enhance".center
+"enhance".center(20)
 => "    enhance    "
-.lsut
+.ljust
 
-"Stop! You’re under arrest!".capitalize
+"Stop! You’re under arrest!".upcase
 => "STOP! YOU’RE UNDER ARREST!"
 
-"the usual" + suspects.to_s
+"the usual" + " suspects".to_s
 => "the usual suspects"
 .replace
 
-" suspects".replace
-=> "the usual suspects"
+# " suspects".
+# => "the usual suspects"
 
 "The case of the disappearing last letter".chop
 => "The case of the disappearing last lette"
@@ -30,16 +30,16 @@
 .delete
 
 
-"The mystery of the missing first letter".delete
+"The mystery of the missing first letter".delete "T"
 => "he mystery of the missing first letter"
 
 "Elementary,    my   dear        Watson!".strip
 => "Elementary, my dear Watson!"
 
-"z".gsub
+"z".ord
 => 122
-.sub
 # (What is the significance of the number 122 in relation to the character z?)
+no clue....
 
 "How many times does the letter 'a' appear in this string?".count "a"
 => 4
