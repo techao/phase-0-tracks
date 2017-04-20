b32a39f36fe065ef590ded0c8f0908c702feaf87
Thomas Chao <techao@gmail.com>
2017-04-20 04:09:27 -0700

    Reorganize code and Add Boolean for Variables

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,16 +7,16 @@
 #opt for company health insurance?
 
 def vamp_dect
-puts "What is your name?"
-  suspect_name = gets.chomp
-puts "How old are you?"
-  suspect_age = gets.chomp.to_i
-puts "What year were you born?"
-  birth_year = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you?(y or n)"
-  likes_garlic = gets.chomp
-puts "Would you like to enroll in the company’s health insurance? (y or n)"
-  health_insurance = gets.chomp
+  puts "What is your name?"
+    suspect_name = gets.chomp
+  puts "How old are you?"
+    suspect_age = gets.chomp.to_i
+  puts "What year were you born?"
+    birth_year = gets.chomp.to_i
+  puts "Our company cafeteria serves garlic bread. Should we order some for you?(y or n)"
+    likes_garlic = gets.chomp
+  puts "Would you like to enroll in the company’s health insurance? (y or n)"
+    health_insurance = gets.chomp
 
 
 # assign variable to 
@@ -28,21 +28,34 @@ puts "Would you like to enroll in the company’s health insurance? (y or n)"
 
 # if suspect age is correct and wants garlic bread, 
 # then probably not a vampire.
-if suspect_age == (2017 -  birth_year)
-  suspect_age = true
-else 
-  suspect_age = false
-end
-if likes_garlic == "y"
-  likes_garlic = true
-else 
-  likes_garlic = false
-end
-if suspect_age && likes_garlic
-  p "Probably not a vampire."
-end
-end
+  if suspect_age == (2017 -  birth_year)
+    suspect_age = true
+  else 
+    suspect_age = false
+  end
+  if likes_garlic == "y"
+    likes_garlic = true
+  else 
+    likes_garlic = false
+  end
+    if health_insurance == "y"
+    health_insurance = true
+  else
+    health_insurance =false
+  end
+
+  # if suspect age is correct and wants garlic bread, 
+# then probably not a vampire.
+  if suspect_age && likes_garlic
+    p "Probably not a vampire."
+  end
 # age wrong and waives insurance means p "probably a vampire"
+
+
+  if 
+   
+  end 
+ end
 
 # age wrong + doesnt want garlic + waives healthy
 # p "almost certaiinly a vamp."
