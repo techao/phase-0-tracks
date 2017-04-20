d97990199562ab24eb81603342559b1fa5d848cd
Thomas Chao <techao@gmail.com>
2017-04-20 03:31:18 -0700

    Assign Variable Name to User Inputs

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,11 +7,15 @@
 #opt for company health insurance?
 
 puts "What is your name?"
-
+  suspect_name = gets.chomp
 puts "How old are you?"
+  suspect_age = gets.chomp
 puts "What year were you born?"
+  birth_year = gets.chomp.to_i
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  garlic = gets.chomp
 puts "Would you like to enroll in the company’s health insurance?"
+  health_insurance = gets.chomp
 
 # assign variable to 
   # -name
