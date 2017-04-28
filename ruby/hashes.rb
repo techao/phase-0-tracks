60394be9884efe254daed30af3d86431d2732721
Thomas Chao <techao@gmail.com>
2017-04-28 02:44:26 -0700

    Add Code for User Input

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -19,16 +19,21 @@ room_count: ""
 }
 
 puts "Name?"
+design_project[:name] = gets.chomp
 
 puts " Age?"
+design_project[:age] = gets.chomp
 
 puts "Enter number of children."
+design_project[:child_count] = gets.chomp
 
 puts "Enter number of windows."
+design_project[:window_count] = gets.chomp
 
 puts "Enter type of house."
+design_project[:house_type] = gets.chomp
 
 puts "Enter number of rooms."
-
+design_project[:house_type] = gets.chomp
 
 
