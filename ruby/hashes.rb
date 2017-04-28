9a3f114520c6111be883f4eaf40cab7c054ff849
Thomas Chao <techao@gmail.com>
2017-04-28 02:35:50 -0700

    Psuedocode for 5.2 Release 2

---
+++ ruby/hashes.rb
@@ -0,0 +1,12 @@
+# create an array to hold all the info for job app
+
+# prompt user for name
+# prompt user age
+# prompt user for number of children
+# prompt user for decor theme
+# prompt user for number of windows
+# prompt user for type of house
+# prompt user for number of rooms
+
+
+
