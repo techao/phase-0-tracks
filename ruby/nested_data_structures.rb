45bbc4f4b25e62dac4cb42b2a00b72d89a3eea9f
Thomas Chao <techao@gmail.com>
2017-04-30 14:04:30 -0700

    Create 3 Arrays Nested in a DrinkMenu Hashin Nested in Menu Hash and an Empty Food Array Nested in the Same Hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,12 +2,31 @@
 
 
 # menu
+
 # drink menu
+teaspoon_menu{
+
+	drink_menu: {
+
+	specialty_drinks: ["House Milk Tea", "Taro Lover",
+		"Mango Funtime", "Classic Chai Tea"] 
+
+	snow_specials: ["California Speacial", "Nutty Taro",
+	"Hangover 3", "Afternoon 3"]
+
+	strawberry_specials: ["strawberry sangria", "strawberry velvet"]}
+
+	food_menu: []
+}
+
+
+
 	# specialty drinks
 		# house milk tea
 		# taro lvover
 		# mango funtime 
 		# classic chai tea
+
 
 	# snow_ice_specials
 		# callifornia snow
