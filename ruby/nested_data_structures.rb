921ff2547714fb839a96f5657b4ca76021be595c
Thomas Chao <techao@gmail.com>
2017-04-30 13:53:23 -0700

    Pseudocode for Random Boba Menu

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,17 @@
+# Teaspoon meanu
+
+# specialty drinks
+	# house milk tea
+	# taro lvover
+	# mango funtime 
+	# classic chai tea
+
+# snow_ice_specials
+	# callifornia snow
+	# nutty taro
+	#hangover 3
+	# afternoon tea
+
+# strawberry serries 
+	# strawberry sangria
+	# strawberry velvet
