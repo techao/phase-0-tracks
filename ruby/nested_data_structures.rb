98505fa79950f2332c6c4386a31602ba4a12c614
Thomas Chao <techao@gmail.com>
2017-04-30 13:56:32 -0700

    Add Menu Hash , Nested Drink Hash in Menu Hash, Empty Array in Menu Hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,17 +1,22 @@
 # Teaspoon meanu
 
-# specialty drinks
-	# house milk tea
-	# taro lvover
-	# mango funtime 
-	# classic chai tea
 
-# snow_ice_specials
-	# callifornia snow
-	# nutty taro
-	#hangover 3
-	# afternoon tea
+# menu
+# drink menu
+	# specialty drinks
+		# house milk tea
+		# taro lvover
+		# mango funtime 
+		# classic chai tea
 
-# strawberry serries 
-	# strawberry sangria
-	# strawberry velvet
+	# snow_ice_specials
+		# callifornia snow
+		# nutty taro
+		#hangover 3
+		# afternoon tea
+
+	# strawberry serries 
+		# strawberry sangria
+		# strawberry velvet
+# non existent food menu
+
