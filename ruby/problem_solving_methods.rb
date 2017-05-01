42dc3cf845fdfa71b39e62336e377347a6f88cee
Thomas Chao <techao@gmail.com>
2017-05-01 10:08:30 -0700

    Include Psuedocode for Method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,3 +1,36 @@
-search_array = [42, 89, 23, 1]
-p search_array(search_array, 23)
+arr_2 = [3, 4, 10]
 
+def search_array(array, rando_int)
+	array.include?(rando_int)
+	if true
+		array.index(rando_int)
+else 
+  nil
+	end
+end
+ p sarch_array(arr_2, 4)
+
+
+# method 
+# define
+# define search array
+
+
+# def
+#  search_array(array, rando_int) 
+
+# array.include?(rando_int)
+
+# if true
+# return index of rando_int
+# 	end 
+# end
+
+# ----------------- Goal-Result/Return
+
+# arr_1 = [42, 89, 23, 1, 12, 9, 5, 10]
+# arr_2 = [3, 4, 10]
+# search_array(arr_1, 22)
+# ==>nil
+# search_array(arr_1, 1)
+# ==>3
