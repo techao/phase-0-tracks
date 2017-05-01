4642cf2c78b06cbaed4967fa39781d0c96b131a4
Thomas Chao <techao@gmail.com>
2017-04-30 18:24:44 -0700

    Add Psuedocode for firt 2 parts of 5.5 Release0

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,17 @@
+# prompts user for name and takes in as string
+#takes name and swaps first and last name
+
+# takes all vowels in name and replaces each existing vowel that is
+# a character and replaces it with the the vowel next in the ordered list,
+# aeiou. example: reaplce 'a' with 'e' and 'u' with 'a'.
+# will need array or hash with 
+# 
+
+
+
+aeiou
+
+# while also doing the same with the consants.
+
+bcdfghjklmnpqrstvwxz
+
