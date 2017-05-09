c344b70e2bbdd78ec92655a63fc28e576ba796a8
Thomas Chao <techao@gmail.com>
2017-05-09 02:19:30 -0700

    Add Code Prompting User for All Necessary Info

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,1 +1,6 @@
+puts "What is your name?"
+puts "How old are you?"
+puts "What year were you born"
+puts "Would you like us to order you garlic bread from the cafe?"
+puts "Would you like to enroll in our health insurance?"
 
