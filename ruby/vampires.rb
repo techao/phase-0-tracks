76b14f931a397ff86c5188fd04e09ffd97ac037c
Thomas Chao <techao@gmail.com>
2017-05-09 15:40:24 -0700

    Update Case Condition for Vampire Detector Logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,43 +8,41 @@ puts "What year were you born"
   birth_year = gets.chomp.to_i
 
 puts "Would you like us to order you garlic bread from the cafe? (y or n)"
-  like_garlic = gets.chomp
+  order_garlic = gets.chomp
 
 puts "Would you like to enroll in our health insurance?(y or n)"
-  health_ins = gets.chomp
+  buy_ins = gets.chomp
+
 
 if 2017 - birth_year == emp_age
-  emp_age = true
-else
-  emp_age = false
+  age_match = true
 end
 
-if like_garlic == "y"
+if order_garlic == "y"
   like_garlic = true
-else
-  like_garlic = false
 end
 
-if health_ins == "y"
+if buy_ins == "y"
   health_ins = true
-else
-  health_ins = false
 end
 
 # do the thing
 
 case 
-when emp_age && (like_garlic || health_ins)  
-  "Probably not a vampire."
-when !emp_age && (!like_garlic || !health_ins) 
- "Probably a vampire."
-when !emp_age && (!like_garlic &&  !health_ins)
- "Almost certainly a vampire."
-when emp_name == "Drake Cula" or "Tu Fang" 
- "Definitely a vampire."
-else 
-  "Results inconclusive "
+  when emp_name == "Drake Cula" 
+    puts "Definitely a vampire."
+  when emp_name == "Tu Fang"
+    puts "Definitely a vampire."
+  when !age_match && !like_garlic && !health_ins
+    puts "Almost certainly a vampire."
+  when age_match && (like_garlic || health_ins)  
+    puts "Probably not a vampire."
+  when !age_match && (!like_garlic || !health_ins) 
+    puts "Probably a vampire."
+  else
+    puts "Inconclusive results"
 end
+
 
 
 
