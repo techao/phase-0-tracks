374320aa608b953071afde7c7e007a3b03f880a6
Thomas Chao <techao@gmail.com>
2017-05-09 02:34:27 -0700

    Add Vairbales for Each Bit of Info and conditions for Age and Garlic Variable

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,6 +1,27 @@
 puts "What is your name?"
+  emp_name = gets.chomp
+
 puts "How old are you?"
+  emp_age = gets.chomp.to_i
+
 puts "What year were you born"
-puts "Would you like us to order you garlic bread from the cafe?"
-puts "Would you like to enroll in our health insurance?"
+  birth_year = gets.chomp.to_i
+
+puts "Would you like us to order you garlic bread from the cafe? (y or n)"
+  like_garlic = gets.chomp
+
+puts "Would you like to enroll in our health insurance?(y or n)"
+  health_ins = gets.chomp
+
+if birth_year - 2017 = emp_age
+  age_match - true
+end
+
+if like_garlic == "y"
+  like_garlic = true
+else
+  like_garlic = false
+end
+
+
 
