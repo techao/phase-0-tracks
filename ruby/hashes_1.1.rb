8ba0900faeaa296ba261f8032b4e55ce16ac2dda
Thomas Chao <techao@gmail.com>
2017-05-11 20:42:47 -0700

    Write Out Pseudocode for Designer Application

---
+++ ruby/hashes_1.1.rb
@@ -0,0 +1,18 @@
+# prompt user for name
+# prompt user for address
+# prompt user for email
+# prompt user for phone number
+
+# prompt user to pick wall paper preferences given the following
+# ▢ Paisley
+# ▢ Chevrons
+# ▢ Photorealistic woodsy scenes (with or without squirrels)
+# ▢ Abstract woodsy scenes (no squirrels)
+
+
+# prompt user to define 'Ombre' given the following options:
+# ◯ Fierce
+# ◯ So last season
+# ◯ Practically medieval in its appalling irrelevance
+
+
