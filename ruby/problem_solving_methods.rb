97f59fd68bbe4bbfee92ee67545e5db1a9f4c7c0
Thomas Chao <techao@gmail.com>
2017-05-22 07:18:04 -0700

    Write New Method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,42 +1,14 @@
-arr_2 = [3, 4, 10]
-
-def search_array(array, rando_int)
-	array.include?(rando_int)
-	if true
-		array.index(rando_int)
-else 
-  nil
-	end
-end
- p sarch_array(arr_2, 4)
-
-def search_array(array, interger)
-	array.each_with_index do | ar_numb, index |
-	if interger == ar_numb
-		return index
-		end
-	end
-end
-
- p search_array(arr_2, 4)
-
-
-
-# method 
-# define
-# define search array
-
-
-# def
-#  search_array(array, rando_int) 
-
-# array.include?(rando_int)
-
-# if true
-# return index of rando_int
-# 	end 
+# VVVV  Original code from pairing VVVV
+# arr_2 = [3, 4, 10]
+# def search_array(array, rando_int)
+# 	array.include?(rando_int)
+# 	if true
+# 		array.index(rando_int)
+# else 
+#   nil
+# 	end
 # end
-
+#  p sarch_arra
 # ----------------- Goal-Result/Return
 
 # arr_1 = [42, 89, 23, 1, 12, 9, 5, 10]
@@ -44,4 +16,21 @@ end
 # search_array(arr_1, 22)
 # ==>nil
 # search_array(arr_1, 1)
-# ==>3
+# ==>3y(arr_2, 4)
+# ---------------------
+# VVVV  New Code VVVV
+
+def search_array(arr, x)
+	index = 0
+	while index < arr.length
+	 arr.each
+		if arr[index] == x
+			return index  
+			break
+		end
+		index += 1
+	end	
+end
+
+
+
