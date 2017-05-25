939c0220de986be94cfbb791b70b6ad3b56ef637
Thomas Chao <techao@gmail.com>
2017-05-24 20:06:02 -0700

    Comment Out Psuedocode

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,15 +1,15 @@
-breed -----------
-Huskey /  Pomeranian
+# breed -----------
+# Huskey /  Pomeranian
 
-Physical Traits  -----------
-coat / fur color: "grey & white" 
-size: small 
-weight: 16lbs
-Ears: pointy
-face: narrow 
-tail: short
+# Physical Traits  -----------
+# coat / fur color: "grey & white" 
+# size: small 
+# weight: 16lbs
+# Ears: pointy
+# face: narrow 
+# tail: short
 
-Personlity/Demaonor
-temperment: friendly 
-energy level: high
-barks: moderately 
+# Personlity/Demaonor
+# temperment: friendly 
+# energy level: high
+# barks: moderately 
