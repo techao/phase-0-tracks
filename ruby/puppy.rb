dce37442242f6725da5b56dd3c85c1abe0a81eec
Thomas Chao <techao@gmail.com>
2017-05-24 20:02:21 -0700

    Blank Puppy Ruby File

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,17 +1,2 @@
-breed -----------
-Huskey /  Pomeranian
-
-Physical Traits  -----------
-coat / fur color: "grey & white" 
-size: small 
-weight: 16lbs
-Ears: pointy
-face: narrow 
-tail: short
-
-Personlity/Demaonor
-temperment: friendly 
-energy level: high
-barks: moderately 
 
 
