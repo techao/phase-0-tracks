6e7be4ef60a5ee760c3028584f5890541c9a93b2
Thomas Chao <techao@gmail.com>
2017-05-27 20:55:37 -0700

    Add Dog Years Converter

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -13,10 +13,23 @@ rover.fetch("ball")
 		speak(many_times.to_i)
 		puts "Woof" * many_times
 	end
-	
+
 	def roll_over
 		puts "rolls over"
 	end
+
+	def dog_years(h_age)
+		if h_age.to_i < 0
+			print "Age must be positive number."
+  		elsif h_age.to_i <= 2
+	  		d_age = h_age * 10.5
+  		else
+	  		d_age = 21 + (h_age.to_i - 2)*4
+		end
+		print "The dog's age in dog's years is, #{d_age}"
+	end 
+
+end
 
 
 
