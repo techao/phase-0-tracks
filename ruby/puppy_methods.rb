dec5e824b243afc778403e48a4fa4864a42066cd
Thomas Chao <techao@gmail.com>
2017-05-27 20:52:47 -0700

    Add Speak method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,8 +4,16 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
 end
 
 rover = Puppy.new
 rover.fetch("ball")
+
+	def 
+		speak(many_times.to_i)
+		puts "Woof" * many_times
+	end
+
+
+
+
