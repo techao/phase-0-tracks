4d7644c8920908a8b0a28d1b9ac64adf99e1c85f
Thomas Chao <techao@gmail.com>
2017-05-31 11:07:29 -0700

    Add Greetings, Eat Cookie, and Initialize Methods

---
+++ ruby/santa.rb
@@ -0,0 +1,21 @@
+# A speak method that will print "Ho, ho, ho! Haaaappy holidays!" (Santa has been working on being more inclusive.)
+# An eat_milk_and_cookies method that takes a cookie type (example: "snickerdoodle") as a parameter and prints "That was a good <type of cookie here>!" 
+# An initialize method that prints "Initializing Santa instance ...".
+
+
+
+class Santa
+
+def greetings
+	puts "Ho,ho, ho! Happy holidays!"	
+end
+
+def eat_milk_and_cookies(cookie)
+ puts "That was a a good #{cookie}!"
+end
+
+def initialize
+	"Initializing Santa instance..."
+end
+
+end
