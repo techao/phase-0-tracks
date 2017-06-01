22746724ed28101792f8df7adbdbdaf355c20736
Thomas Chao <techao@gmail.com>
2017-06-01 16:35:05 -0700

    Add Age Method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -12,36 +12,69 @@
 
 
 class Santa
-def initialize(gender, ethnicity)
-	puts "Initializing Santa instance..."
-	@gender = gender
-	@ethniciti = ethnicity
-		@age = age = 0
+
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethniciti = ethnicity
+		@age = age.to_i
+		end
+
+
+	def reindeer_ranking
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	end
+
+	def greetings
+		puts "Ho,ho, ho! Happy holidays!"	
+		end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie} cookie!"
+		end
+
+	def celebrate_birthday
+
+	def age
+   		@age 
+		end
+
+	def ethnicity
+	  	@ethnicity
+		end
+
+	def celbday
+	  	@age += 1
+		end
+
+	def get_mad(reindeer)
+		reindeer_ranking.delete_if {|name| name.to_s == reindeer }
+	end
+
 end
 
-def greetings
-	puts "Ho,ho, ho! Happy holidays!"	
-end
+	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 
-def eat_milk_and_cookies(cookie)
- puts "That was a good #{cookie} cookie!"
-end
-
-reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-
+	
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 end
 
 rob = Santa.new
 rob.greetings
 rob.eat_milk_and_cookies('oatmeal')
+rob.age
+rob.celbday
+
 
 santas = []
 genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-
 genders.length.times do |i|
   santas << Santa.new(genders[i], ethnicities[i])
 end
+
+
 
 
 
