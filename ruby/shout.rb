f095c11bf46d787820bfc443eaa57b7a88467655
Thomas Chao <techao@gmail.com>
2017-06-02 09:22:29 -0700

    Add Yelling Happily Method

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
   # we'll put some methods here soon, but this code is fine for now!
 end
+
+module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yelling_happily(words2)
+  	words2 + "!!!" + "|_^o^_/ "
+  end
+end
