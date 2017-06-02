16676b3c79928827d66f0c86c073ca77dbcce468
Thomas Chao <techao@gmail.com>
2017-06-02 09:57:37 -0700

    Correct Syntax, Module Code Move to Top

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -1,12 +1,3 @@
-class Bird
-  include Flight
-end
-
-class Plane
-  include Flight
-endPlane
-end
-
 
 module Flight
   def take_off(altitude)
@@ -14,8 +5,17 @@ module Flight
   end
 end
 
+class Bird
+  include Flight
+end
+
+class Plane
+  include Flight
+end
+
 bird = Bird.new
 bird.take_off(800)
 
 plane = Plane.new
 plane.take_off(30000)
+
