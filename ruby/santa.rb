91b5af57dc92889cf11cbb98a5df7163ade5b738
Thomas Chao <techao@gmail.com>
2017-06-01 18:40:22 -0700

    Refractor Setter Method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,7 @@
 class Santa
-	attr_reader :name, :age, :ethnicity, :gender, :reindeer_ranking
+	attr_reader :name, :age, :ethnicity, :reindeer_ranking
+	attr_accessor :gender
+	
 
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
@@ -17,29 +19,26 @@ class Santa
 		puts "That was a good #{cookie} cookie!"
 	 end
 
-	def celebrate_bday
+	def cel_bday
 	  	@age += 1
+	  	puts age
 	 end
 
 	def get_mad_at(reindeer)
 		reindeer_ranking.delete_if {|name| name.to_s == reindeer }
 		reindeer_ranking.push(reindeer)
 	 end
-
-	def gender=(new_gender)
-		@gender = new_gender
-	end
 end
 
 
 
 
 # Driver Code
-rob = Santa.new
+rob = Santa.new('male','asian')
 rob.greetings
 rob.eat_milk_and_cookies('oatmeal')
 rob.age
-rob.celbday
+rob.cel_bday
 rob.get_mad_at("Rudolph")
 rob.get_mad_at("Vixen")
 rob.get_mad_at("Comet")
