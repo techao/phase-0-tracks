37adb8e07a5987539346055a766d60d2221ee6e4
Thomas Chao <techao@gmail.com>
2017-06-01 17:15:08 -0700

    Add Reindeer Re-ranking Method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,16 +1,3 @@
-# A speak method that will print "Ho, ho, ho! Haaaappy holidays!" (Santa has been working on being more inclusive.)
-# An eat_milk_and_cookies method that takes a cookie type (example: "snickerdoodle") as a parameter and prints "That was a good <type of cookie here>!" 
-# An initialize method that prints "Initializing Santa instance ...".
-
-
-# Update your Santa class with the following attributes:
-
-# gender, which will be a string passed in on initialization
-# ethnicity, which will be a string passed  in on initialization
-# reindeer_ranking, an array of reindeer from most preferred to least preferred. This is not passed in on initialization; it simply gets a default value of ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-# age, which is not passed in on initialization and defaults to 0
-
-
 class Santa
 
 	def initialize(gender, ethnicity)
@@ -18,53 +5,50 @@ class Santa
 		@gender = gender
 		@ethniciti = ethnicity
 		@age = age.to_i
-		end
-
-
-	def reindeer_ranking
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
 
 	def greetings
 		puts "Ho,ho, ho! Happy holidays!"	
-		end
+	 end
 
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie} cookie!"
-		end
-
-	def celebrate_birthday
+	 end
 
 	def age
    		@age 
-		end
+	 end
 
 	def ethnicity
 	  	@ethnicity
-		end
+	 end
 
-	def celbday
+	def celebrate_bday
 	  	@age += 1
-		end
+	 end
 
-	def get_mad(reindeer)
+	def reindeer_ranking
+		@reindeer_ranking
+	 end
+
+	def get_mad_at(reindeer)
 		reindeer_ranking.delete_if {|name| name.to_s == reindeer }
-	end
+		reindeer_ranking.push(reindeer)
+	 end
 
 end
 
-	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-
-	
-genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-end
-
+# Driver Code
 rob = Santa.new
 rob.greetings
 rob.eat_milk_and_cookies('oatmeal')
 rob.age
 rob.celbday
+rob.get_mad_at("Rudolph")
+rob.get_mad_at("Vixen")
+rob.get_mad_at("Comet")
+
 
 
 santas = []
