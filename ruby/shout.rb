437de8289329dec72eca4e852abf5b688c3e7ef9
Thomas Chao <techao@gmail.com>
2017-06-02 09:48:35 -0700

    Add Driver Code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -7,3 +7,9 @@ module Shout
   	words2 + "!!!" + "|_^o^_/ "
   end
 end
+
+# Driver Code
+puts Shout.yelling_happily ('I just won the lotto')
+
+Shout.yell_angrily('But now I have to pay texes on it')
+
