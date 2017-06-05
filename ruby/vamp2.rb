1a2a381c95e4cf9fe98afa46ba8e5480adf153c8
Thomas Chao <techao@gmail.com>
2017-06-05 07:10:28 -0700

    Add Name Logic and Inconclusive Logic

--- ruby/vamp2.rb
+++ ruby/vamp2.rb
@@ -13,12 +13,6 @@ garlic_bread = gets.chomp
 puts "Would yout like to enroll in the company's health insurance? (yes or no)"
 insurance = gets.chomp
 
-# If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
-# If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
-# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
-# Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
-# Otherwise, print “Results inconclusive.
-
 if 2017 - birth_year = vamp_age
 	age_matches = true 
 end
@@ -41,11 +35,16 @@ end
 
 
 case 
-when age_matches && (likes_garlic || health_insurance)  
-        puts "Probably not a vampire."
-when !age_matches && (!like_garlic || !health_ins) 
-        puts "Probably a vampire."
-else !age_matches && [!likes_garlic && !health_insurance]
-        puts "Almost certainly a vampire."
-
+	when age_matches && (likes_garlic || health_insurance)  
+	        puts "Probably not a vampire."
+	when !age_matches && (!like_garlic || !health_ins) 
+	        puts "Probably a vampire."
+	when !age_matches && [!likes_garlic && !health_insurance]
+	        puts "Almost certainly a vampire."
+	when vamp_name == "Drake Cula" 
+	        puts "Definitely a vampire."
+	when vamp_name == "Tu Fang"
+	        puts "Definitely a vampire."
+	else 
+		puts "Inconclusive results."
 end
