ecd72c2c9857fe2b700f742609199c22c19ad60a
Thomas Chao <techao@gmail.com>
2017-06-05 07:13:03 -0700

    Add Multiple Employee Condition

--- ruby/vamp2.rb
+++ ruby/vamp2.rb
@@ -1,50 +1,58 @@
-puts "What is your name?"
-vamp_name = gets.chomp
+   puts "How many employees will you be processing?"
+    count = 0
+    employee_count = gets.chomp.to_i
 
-puts "How old are you?"
-vamp_age = gets.chomp.to_i
+until count == employee_count
 
-puts "What year were you born?"
-birth_year = gets.chomp.to_i
+	puts "What is your name?"
+	vamp_name = gets.chomp
 
-puts "Our cafeteria serves garlic bread. Should we order some for you? (yes or no)"
-garlic_bread = gets.chomp
+	puts "How old are you?"
+	vamp_age = gets.chomp.to_i
 
-puts "Would yout like to enroll in the company's health insurance? (yes or no)"
-insurance = gets.chomp
+	puts "What year were you born?"
+	birth_year = gets.chomp.to_i
 
-if 2017 - birth_year = vamp_age
-	age_matches = true 
-end
+	puts "Our cafeteria serves garlic bread. Should we order some for you? (yes or no)"
+	garlic_bread = gets.chomp
 
-if garlic_bread == "yes"
-	likes_garlic = true 
-elsif garlic_bread == "no"
-	likes_garlic = false
-else
-	likes_garlic = nil
-end
+	puts "Would yout like to enroll in the company's health insurance? (yes or no)"
+	insurance = gets.chomp
 
-if insurance == "yes"
-	health_insurance = true 
-elsif insurance == "no"
-	health_insurance = false
-else
-	health_insurance = nil
-end
+	if 2017 - birth_year = vamp_age
+		age_matches = true 
+	end
+
+	if garlic_bread == "yes"
+		likes_garlic = true 
+	 elsif garlic_bread == "no"
+		likes_garlic = false
+	 else
+		likes_garlic = nil
+	end
+
+	if insurance == "yes"
+		health_insurance = true 
+	 elsif insurance == "no"
+		health_insurance = false
+	 else
+		health_insurance = nil
+	end
 
 
-case 
-	when age_matches && (likes_garlic || health_insurance)  
-	        puts "Probably not a vampire."
-	when !age_matches && (!like_garlic || !health_ins) 
-	        puts "Probably a vampire."
-	when !age_matches && [!likes_garlic && !health_insurance]
-	        puts "Almost certainly a vampire."
-	when vamp_name == "Drake Cula" 
-	        puts "Definitely a vampire."
-	when vamp_name == "Tu Fang"
-	        puts "Definitely a vampire."
-	else 
-		puts "Inconclusive results."
-end
+	case 
+		when age_matches && (likes_garlic || health_insurance)  
+		        puts "Probably not a vampire."
+		when !age_matches && (!like_garlic || !health_ins) 
+		        puts "Probably a vampire."
+		when !age_matches && [!likes_garlic && !health_insurance]
+		        puts "Almost certainly a vampire."
+		when vamp_name == "Drake Cula" 
+		        puts "Definitely a vampire."
+		when vamp_name == "Tu Fang"
+		        puts "Definitely a vampire."
+		else 
+			puts "Inconclusive results."
+	end
+	count += 1
+	
