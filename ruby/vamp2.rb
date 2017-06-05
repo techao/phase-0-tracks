3c2b7dcb326f40a6970e93b2f984eaf8498c792c
Thomas Chao <techao@gmail.com>
2017-06-04 18:26:51 -0700

    Add Code Like Garlic and Want Helath Insurance?

--- ruby/vamp2.rb
+++ ruby/vamp2.rb
@@ -6,3 +6,10 @@ vamp_age = gets.chomp.to_i
 
 puts "What year were you born?"
 birth_year = gets.chomp.to_i
+
+puts "Our cafeteria serves garlic bread. Should we order some for you? (yes or no)"
+garlic_bread = gets.chomp
+
+puts "Would yout like to enroll in the company's health insurance? (yes or no)"
+insurance = gets.chomp
+
