ef89a56a4a8175be56f68b832baa166e6b52a182
Thomas Chao <techao@gmail.com>
2017-06-05 08:28:04 -0700

    Use Test Driver Code and Add User Interface Code

--- ruby/secret_agents2.rb
+++ ruby/secret_agents2.rb
@@ -1,4 +1,7 @@
+
+
 def encrypt(password_str)
+	index = 0
 	while index < password_str.length 
 		password_str[index] = password_str[index].next
 		index +=1
@@ -19,3 +22,27 @@ def decrypt(encrypted_str)
 	end
 	encrypted_str
 end
+
+# puts encrypt("abc") 
+# puts encrypt("zed") 
+# puts decrypt("bcd") 
+# puts decrypt("afe") 
+
+# puts decrypt(encrypt("swordfish"))
+# this nested method call works because it is calling each method one after the other and it doesnt need to access that would otherwise not be available to it becuase these are two seperate methods that dont share local variables.
+
+puts "Hello Agent, please enter a password"
+pw_input = gets.chomp
+
+puts "Would you like to encrypt or decrypt this password? (Please type either 'encrypt' for encryption or 'decrypt' for decryption.)"
+encrypt_decrypt = gets.chomp
+
+case
+when encrypt_decrypt == "encrypt"
+	puts encrypt(pw_input)
+when encrypt_decrypt == "decrypt"
+	puts decrypt(pw_input)
+else 
+	puts "invalid entry or request"
+end
+
