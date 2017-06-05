f0678f463742b40c2cfea1d90efda27b336ecfa8
Thomas Chao <techao@gmail.com>
2017-06-04 18:37:38 -0700

    Begin Adding Conditions and Relate to Case Conditional

--- ruby/vamp2.rb
+++ ruby/vamp2.rb
@@ -13,3 +13,17 @@ garlic_bread = gets.chomp
 puts "Would yout like to enroll in the company's health insurance? (yes or no)"
 insurance = gets.chomp
 
+# If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
+# If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
+# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+# Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+# Otherwise, print “Results inconclusive.
+
+if 2017 - birth_year = vamp_age
+	age_matches = true end
+if garlic_bread
+
+case 
+when condition
+	
+end
