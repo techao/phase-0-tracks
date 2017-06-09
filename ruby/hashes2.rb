368dc34828f19c3f0c700aff74a132ab00f4130d
Thomas Chao <techao@gmail.com>
2017-06-09 16:40:18 -0700

    Pseudocode and Add Hash for Project

---
+++ ruby/hashes2.rb
@@ -0,0 +1,46 @@
+# prompt user for name
+# prompt user for address
+# promopt user for email
+# prompt user for email
+
+
+# prompt user to pick wall paper preferences given the following
+# ▢ Paisley
+# ▢ Chevrons
+# ▢ Photorealistic woodsy scenes (with or without squirrels)
+# ▢ Abstract woodsy scenes (no squirrels)
+
+
+
+# prompt user to define 'Ombre' given the following options:
+# ◯ Fierce
+# ◯ So last season
+# ◯ Practically medieval in its appalling irrelevance
+
+# Release 2...
+
+# prompt user for potential client name
+# prompt user for age of client
+
+# prompt for client building-type
+# prompt for window count
+# prompt for number for stories
+# prompt for approx square footage
+# prompt for number of rooms in propsal
+# prompt for for approx max budget for cleint is willing to pay 
+# prmopt reason for seeking design
+
+client_proj = {
+	name: ''
+	age: ''
+	building_type: ''
+	window_count: ''
+	story_count: ''
+	square_ft:''
+	max_bduget: ''
+	proj_purpose: ''
+}
+
+
+
+
