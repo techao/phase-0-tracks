b31fa65b7a9b9aef5906a90b27d0c1b8dfd141fc
Thomas Chao <techao@gmail.com>
2017-06-09 20:46:26 -0700

    Correct Typo

--- ruby/nested_data_structures2.rb
+++ ruby/nested_data_structures2.rb
@@ -65,7 +65,7 @@ non_coffee_drinks: ['Gibraltar Hot Chocolate', 'Fruit Juice', 'Sodas', 'Energy D
 
 food_items: ['Quiche Loraine', 'Daily Vegetarian Special']
 
-deserts: 'Hnad Crafted Daily Special '
+desserts: 'Hnad Crafted Daily Special '
 
 }
 
