8e6094e03485c2735145983511f7196bb43cd40c
Thomas Chao <techao@gmail.com>
2017-06-09 19:19:48 -0700

    Suedo Code Coffee Shop Menu for Data Strucure

---
+++ ruby/nested_data_structures2.rb
@@ -0,0 +1,55 @@
+# ---Menu----
+# Drip Coffee FETCO Brewed 12oz-16oz-20oz
+# Medium and Dark roasts available
+# Decaf
+# Traveler--96oz to-go
+# Espresso Drinks
+# Espresso
+# Macchiato 
+# Gibraltar
+# Cappuccino
+# Americano
+# Latte
+
+# ---Speciality Drinks Hot/Cold---
+# Guittard Mocha Latte 
+# Vanilla Bean Latte
+# Caramel Latte
+# Havana Latte
+# Chai Latte
+# East Bay Iced Latte
+# Nitro Tap- cold brewed coffee infused with nitrogen
+# Nitro-Tap floats Nitro coffee with vanilla ice cream and chocolate shavings
+# ---Tea---
+# Harney and Sons whole leaf selections served individually or in a tea pot
+# Sweet/Un-sweet brewed Iced tea
+
+
+# --Fresh Organic Vegetable Juices---
+# Green Smoothie- Spinach/Kale/Apple/Ginger/Lemon
+# Red Juice- Beet/carrot/ginger/apple 
+
+# ---Non-Coffee Drinks---
+# Guittard Hot Chocolate
+# fruit juices
+# Sodas
+# energy drinks
+# water
+
+# ---Food Items--
+# Quiche Loraine 
+# vegetarian selections 
+
+# ---Desserts---
+# Hand Crafted desserts 
+
+
+
+
+
+
+
+
+
+
+ 
