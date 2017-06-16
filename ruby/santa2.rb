5ae29d8ce66e11159677e9a1cb338f30d1479bda
Thomas Chao <techao@gmail.com>
2017-06-15 23:59:01 -0700

    Add Initialize, Speak and Eat Cookie Methods

--- ruby/santa2.rb
+++ ruby/santa2.rb
@@ -1,2 +1,15 @@
 class Santa
+
+	def initialize
+		puts "Initializing santa instance..."
+	end
+	
+	def speak
+		"Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_n_cookies(cookie)
+		puts "That was a good #{cookie}!" 
+	end
+
 end
