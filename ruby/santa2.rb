72ad8bb1604d240f2be2ba1d2b55ad7ae8378ef9
Thomas Chao <techao@gmail.com>
2017-06-16 00:59:16 -0700

    Add Bday and Get Mad Methods to Santa Class

--- ruby/santa2.rb
+++ ruby/santa2.rb
@@ -1,4 +1,5 @@
 class Santa
+	
 
 	def initialize(gender, ethnicity)
 		puts "Initializing santa instance..."
@@ -14,6 +15,18 @@ class Santa
 	def eat_milk_n_cookies(cookie)
 		puts "That was a good #{cookie}!" 
 	end
+
+	def celebrate_bday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		reindeer_ranking.delete_if {|name| name.to_s == reindeer }
+		reindeer_ranking.push(reindeer)
+	end
+
+
+
 	
 	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 end
@@ -21,4 +34,7 @@ end
 mary = Santa.new('female', 'amernian')
 mary.speak
 mary.eat_milk_n_cookies('chocolate chip cookie')
-# mary.initialize(female, armenian)
+mary.celebrate_bday
+mary.get_mad_at("Rudolph")
+mary.get_mad_at("Vixen")
+mary.get_mad_at("Comet")
