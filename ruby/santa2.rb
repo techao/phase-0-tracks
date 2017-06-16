14413c460b22ce10cb0cedd5f46ae34fa8a3c407
Thomas Chao <techao@gmail.com>
2017-06-16 00:41:55 -0700

    Update Attributes in Sana Class with Gender, Ethnicity, and Reindeer Ranking

--- ruby/santa2.rb
+++ ruby/santa2.rb
@@ -1,19 +1,24 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@age = age = 0
 	end
 
 	def speak
-		"Ho, ho, ho! Haaaappy holidays!"
+		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
 
 	def eat_milk_n_cookies(cookie)
 		puts "That was a good #{cookie}!" 
 	end
-
+	
+	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 end
 
-mary = Santa.new
+mary = Santa.new('female', 'amernian')
 mary.speak
 mary.eat_milk_n_cookies('chocolate chip cookie')
+# mary.initialize(female, armenian)
