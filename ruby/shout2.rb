709d92b50ade9b4e9b4d79d286dbe908e1f21919
Thomas Chao <techao@gmail.com>
2017-06-16 19:45:43 -0700

    Craete Mixin Version of Shout Module and Add Two Classes

--- ruby/shout2.rb
+++ ruby/shout2.rb
@@ -1,14 +1,44 @@
+# module Shout
+# 	 def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#    def self.yelling_happily(words2)
+#   	words2 + "!!!" + "|_^o^_/ "
+#   end
+# end
+
+# # driver code
+
+# puts Shout.yelling_happily ('I am so happy')
+
+# puts Shout.yell_angrily ("Now I'm not")
+
+
 module Shout
-	 def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-   def self.yelling_happily(words2)
-  	words2 + "!!!" + "|_^o^_/ "
-  end
+	def yell_win(words)
+		puts words + "FTW!!!" + '\_^0^_/'
+	end
+	def yell_loss(words2)
+		puts words2 + "Aww well, there's always next time"
+	end
+	def yell_pain(words3)
+		puts words3 + 'Owwwww Owww Owww Owww' + 'T__T'
+	end
+	def yell_taunt
+			puts "You can't handle this"
+	end
+	def yell_curse(words4)
+		puts 'Oh yeah? Well...' + words4 + 'and !@#$%^&#$%^&*'
+	end
+	def yell_sorry
+		puts "It was an accident!"
+	end
 end
 
-# driver code
+class Player1 
+	include Shout 
+end
 
-puts Shout.yelling_happily ('I am so happy')
-
-puts Shout.yell_angrily ("Now I'm not")
+class Player2
+	include Shout
+end
