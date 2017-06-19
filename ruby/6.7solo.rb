d2766a63c69331fbedb6de8995266dc22b4aef4c
Thomas Chao <techao@gmail.com>
2017-06-19 06:27:40 -0700

    Create Word Game rb File and Add Hang_Man Module with  2 Player Class Instances

---
+++ ruby/6.7solo.rb
@@ -0,0 +1,25 @@
+
+# possible module isntead of class
+module Hang_man
+attr_reader :correct_guess
+attr_accessor :word
+
+# possible class instance here
+	def initialize
+		@word = word
+		@guess_count = word.length 
+		@correct_guess = false
+		@alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+	end
+
+end
+
+class Player1
+	include Hang_man
+end
+
+class Player2
+	include Hang_man
+end
+
+
