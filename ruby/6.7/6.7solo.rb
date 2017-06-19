f21a9fd736b636d56ede83042bb0e3227f3727a9
Thomas Chao <techao@gmail.com>
2017-06-19 07:23:37 -0700

    Rewrite Method and Driver Code for Player one andWord

--- ruby/6.7/6.7solo.rb
+++ ruby/6.7/6.7solo.rb
@@ -1,12 +1,10 @@
 
 # possible module isntead of class
 # module Hang_man
-
-
-class word_game
+	class Word_game
 
 attr_reader :correct_guess
-attr_accessor :word
+attr_accessor :word, :guessword
 
 	def initialize
 		@word = word
@@ -15,10 +13,10 @@ attr_accessor :word
 		@alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 	end
 
-	def given_word
-		@word.split('')
+	 def given_word(word)
+	 @guessword = word.to_s.split('')
 	end
-end
+# end
 
 # class Player1
 # 	include Hang_man
@@ -27,5 +25,11 @@ end
 # class Player2
 # 	include Hang_man
 # end
+
+
+player_1 = Player1.new
+puts 'Player 1 enter a word'
+player_1.Word_game.given_word
+
 
 
