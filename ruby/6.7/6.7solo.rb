2d779eb2270d74f06a2fad69ea8f46d60622925b
Thomas Chao <techao@gmail.com>
2017-06-19 06:54:19 -0700

    Split Word into Array Method

--- ruby/6.7/6.7solo.rb
+++ ruby/6.7/6.7solo.rb
@@ -15,6 +15,9 @@ attr_accessor :word
 		@alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 	end
 
+	def given_word
+		@word.split('')
+	end
 end
 
 # class Player1
