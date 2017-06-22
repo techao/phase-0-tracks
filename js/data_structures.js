92c04011ab77a9dd567d80970693c7134f161d42
Thomas Chao <techao@gmail.com>
2017-06-22 16:19:29 -0700

    Update Program To Use Built in Methods Instead of Written Functions

---
+++ js/data_structures.js
@@ -0,0 +1,12 @@
+var horsecolours = ["green ", "blue", "black with yellow stars", "inivisible"];
+var horsenames = ["Igneel", "Gajeel", "Mystogan", "Hajime"];	
+
+
+
+
+horsenames.push("Ed")
+
+horsecolours.push("Forest Gold")
+
+console.log(horsecolours)
+console.log(horsenames)
