c4a0869850599a73780014068ee625f7796c0f8b
Thomas Chao <techao@gmail.com>
2017-06-22 07:09:27 -0700

    Pseudocode Update

--- js/explore.js
+++ js/explore.js
@@ -1,23 +1,23 @@
 // In comments, pseudocode a function that takes a string as a parameter and reverses the string. reverse("hello"), for example, should return "olleh"
 
-// itterate thorugh string 'hello'  
-// go through string and add each letter into an array 
-// access each letter in the array starting with the last element in the array
-// one by one combine into a single string
-// print the new string
+// write a function to reverse a given string
+// decalre a function, call it reverse and allow it to have input 
+// **define a empty string variable in the beggining of function for reversed string to be saved as**
+// go through string and count each character, set the total count of characters in the string to the index variable, or 'i'
+// while 'i' (length of given  string) is greater than the value 1, access the string chracters using its character index starting with the last letter which 
+// the character index of the string is also the length of the string minus -1, given that index starts at 0.
+// declare it in the empthy varaible as as a string.
+// subtract 1 from 'i' to account for the accessed letter and to continue through the string accessing the next character from right to left
+// next letter should be accesed because 'i' is still greater than the value 0 and but a different value due to subtracting 1
+// subsequently add each letter from end to beggining of given string to a new variable.
+// once all letters in string have been accessed, 'i' will = 0 ending the loop, given that i is not longer greater than 0. 
+// subtracting 1 from either the string length variable 'i' or 'i' in the character access method will account for index starting at 0.
+
+
+
+   
 
 
 
 
-public static  String reverse (String a){
-    char[] rarray = a.toCharArray();
-    String finalvalue = "";
-    for (int i = 0; i < rarray.length; i++)
-    {
-        finalvalue += rarray[rarray.length - 1 - i];
-    }   
-return finalvalue;
 
-
-
-String "hello"
