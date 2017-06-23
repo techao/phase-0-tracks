618f0adcde4e5bc74a484fa9b1a2ed8aa811cbd3
Thomas Chao <techao@gmail.com>
2017-06-23 15:28:47 -0700

    GPS Pairing Makeup

--- ruby/gps2_2_1.rb
+++ ruby/gps2_2_1.rb
@@ -25,3 +25,49 @@
 # input:hash list
 # steps: itterate through each key value pair and print each key string ,i.e. item, with its corresponding key value
 # output: String stating number of items needed. 
+
+def make_a_list(str)
+	groecery_list = {}
+	list_arr = str.split
+	list_arr.each do |item| groecery_list[item] = 1
+	end
+	groecery_list
+end
+
+def add_item(grcy_list, string, i=1)
+  grcy_list[string] = i
+end
+
+
+def remove_item(grcy_list, string)
+ grcy_list.delete(string)
+end
+
+def update_quantity(grcy_list, string, i)
+	grcy_list[string] = i  
+end
+
+
+def print_list(grcy_list)
+	grcy_list.each do |item, quantity|
+		puts "You need #{quantity} #{item}"
+	end
+end
+
+
+june_groc_list = make_a_list("avacados oranges potatoes dozeneggs loaf_of_bread")
+puts june_groc_list
+
+add_item(june_groc_list, "apples")
+puts june_groc_list
+
+delete_item(june_groc_list, "oranges")
+puts june_groc_list
+
+remove_item(june_groc_list, "avacadoS")
+puts june_groc_list
+
+update_quantity(june_groc_list, "potatoes", 16)
+puts june_groc_list
+
+print_list(june_groc_list)
