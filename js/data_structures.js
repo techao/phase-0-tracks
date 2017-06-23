27db2426a5c3c6183c4cae2204f8177e394d109d
Thomas Chao <techao@gmail.com>
2017-06-23 14:20:05 -0700

    Update, Incomplete

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,11 +2,14 @@ var horsecolours = ["green ", "blue", "black with yellow stars", "inivisible"];
 var horsenames = ["Igneel", "Gajeel", "Mystogan", "Hajime"];	
 
 
+horsenames.push("Ed");
+horsecolours.push("forest gold");
 
+console.log(horsecolours);
+console.log(horsenames);
 
-horsenames.push("Ed")
+var horses = {};
+for (var i = 0; i < horsecolours.length; i++); {
+ horses[horsenames[i]] = horsecolours[i]); 
+}
 
-horsecolours.push("Forest Gold")
-
-console.log(horsecolours)
-console.log(horsenames)
