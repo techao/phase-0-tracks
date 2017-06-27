4143e92f08c3909d715f4767930b0486f84b45d7
Thomas Chao <techao@gmail.com>
2017-06-26 17:46:02 -0700

    Pass First 3 Tests

--- ruby/dancer/dancer.rb
+++ ruby/dancer/dancer.rb
@@ -1,3 +1,17 @@
 class Dancer
+	attr_reader :name
+	def initialize(name, age)
+		@name =  name
+		@age = age.to_i
 	end
-	
+
+	def pirouette
+		p "*twirls*"
+	end
+
+	def bow
+		p "*bows*"
+	end
+
+end
+
