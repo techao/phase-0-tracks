9613034285d5a4da438518e8728dc332776c7ba0
Thomas Chao <techao@gmail.com>
2017-06-27 17:42:29 -0700

    update

--- ruby/6.7/6.7solomstr.rb
+++ ruby/6.7/6.7solomstr.rb
@@ -1,9 +1,8 @@
 
 class Wordgame
-
 	attr_accessor :letter_index, :given_word, :word_array, :word_to_guess, :max_guesses
-	attr_reader :game_over, :guess_count
-
+  	attr_reader :game_over, :guess_count
+	
 	def initialize
 		@max_guesses = max_guesses.to_i	
 		@guess_count = 0
@@ -15,59 +14,36 @@ class Wordgame
 	end
 
 	def give_word(word)
-<<<<<<< HEAD
 		@given_word = word.to_s
 		@word_array = word.to_s.split('')
 	  	@word_to_guess = word.gsub(/[a-z]/, '_').split('')
 		@max_guesses = given_word.size * 2
 	end
-	
-	def guess_the_letters(letter)
-		@guess_count += 1
-=======
-		 @given_word = word.to_s
-		 @word_array = word.to_s.split('')
-	  	 @word_to_guess = word.gsub(/[a-z]/, '_').split('')
-		 @max_guesses = given_word.size * 2
-	end
+		
 
 	def guess_the_letters(letter)
 	  	@guess_count += 1
->>>>>>> 461b0804a04df22b7cc8cbff6b2b9ce5e6814391
 		if word_array.include?(letter)
 			letter_index = word_array.find_index(letter)
 			word_array[letter_index] = ""
 			word_to_guess[letter_index] = letter
 			word_to_guess.join
 			@game_over = true
-<<<<<<< HEAD
-		else
-			@game_over =  false
-		end
-	end
-
-end
-
-	# user interface
-=======
 		 else
 			@game_over =  false
 		end
-	end
+	 end
 end
 
-
 # user interface
->>>>>>> 461b0804a04df22b7cc8cbff6b2b9ce5e6814391
 puts "initializing new game..."
 newgame = Wordgame.new
 puts 'Player 1 enter a word'
 word = gets.chomp
 newgame.give_word(word)
 
-	# newgame.guess_the_letters(letter)
+# newgame.guess_the_letters(letter)
 
-<<<<<<< HEAD
 
 	while 
 		newgame.guess_count < newgame.max_guesses
@@ -81,17 +57,7 @@ newgame.give_word(word)
 	    puts "The word doesnt contain that letter. Try Again"
 	  end
 	end
-=======
-while newgame.guess_count < newgame.max_guesses
-	puts "Player 2, enter a letter to guess the word"
-	letter_guess = gets.chomP
-	if newgame.guess_the_letters(letter_guess)
-		puts "Yes! The word is now .... #{newgame.word_to_guess.join}"
-	 else !newgame.guess_the_letters(letter_guess)
-		puts "The word doesnt contain that letter. Try Again"
-	end
-end
->>>>>>> 461b0804a04df22b7cc8cbff6b2b9ce5e6814391
+
 
 
 
